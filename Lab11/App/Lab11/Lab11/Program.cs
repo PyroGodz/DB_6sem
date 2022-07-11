76f4fa270e8775c960dc4066d6d58cee4cb57cec
PyroGodz <bad.bug39@gmail.com>
2022-07-11 10:35:42 +0300

    Sem closed

--- Lab11/App/Lab11/Lab11/Program.cs
+++ Lab11/App/Lab11/Lab11/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 
 namespace Lab11
 {
@@ -7,6 +8,31 @@ namespace Lab11
         static void Main(string[] args)
         {
 
+  
+            string FIO, sqlExpression;
+            int payment;
+            string input = Console.ReadLine();
+            while (!input.Equals("q"))
+            {
+
+                    Console.WriteLine("Введите ФИО");
+                    FIO = Console.ReadLine();
+                    Console.WriteLine("Введите заработную плату");
+                    payment = Convert.ToInt32(Console.ReadLine());
+                    sqlExpression = $"insert into coach (FIO, payment) VALUES ({FIO}, {payment});";
+                using (var connection = new SqliteConnection("Data Source=lab11.db"))
+                {
+                    connection.Open();
+                    SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+                    Console.WriteLine("Данные вставлены в таблицу coach");
+                    //    input = Console.ReadLine();
+                    //    command.CommandText = "select * from coach;";
+                    //    command.ExecuteNonQuery();
+            }
+            Console.Read();
         }
     }
 }
---
+++ Lab11/WorkingApp/Lab11/Lab11/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FIO, sqlExpression;
+            int payment;
+            string input = "";
+            Console.WriteLine($"Введите Insert, update или delete");
+            while (!input.Equals("q"))
+            {
+
+                if (input.Equals("Insert"))
+                {
+                    sqlExpression = "INSERT INTO Users (Name, Age) VALUES ('Alice', 32), ('Bob', 28)";
+                    using (var connection = new SqliteConnection("Data Source=lab11.db"))
+                    {
+                        connection.Open();
+
+                        SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                        int number = command.ExecuteNonQuery();
+
+                        Console.WriteLine($"В таблицу Users добавлено объектов: {number}");
+                    }
+                }
+
+
+
+                if (input.Equals("Update"))
+                {
+                    sqlExpression = "UPDATE Users SET Age=20 WHERE Name='Tom'";
+                    using (var connection = new SqliteConnection("Data Source=lab11.db"))
+                    {
+                        connection.Open();
+
+                        SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                        int number = command.ExecuteNonQuery();
+
+                        Console.WriteLine($"Обновлено объектов: {number}");
+                    }
+                }
+                if (input.Equals("Delete"))
+                {
+                    sqlExpression = "DELETE  FROM Users WHERE Name='Tom'";
+                    using (var connection = new SqliteConnection("Data Source=lab11.db"))
+                    {
+                        connection.Open();
+
+                        SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                        int number = command.ExecuteNonQuery();
+
+                        Console.WriteLine($"Удалено объектов: {number}");
+                    }
+                }
+
+               
+
+                else
+                {
+                    Console.WriteLine($"Неверная команда");
+                }
+                input = Console.ReadLine();
+            }
+            Console.Read();
+        }
+    }
+}
